8f0d4535084872fd9cb33518d73bffe326e92c34
Fahim-Ahmed-40 <fahimiit12@gmail.com>
2025-11-17 12:49:48 +0600

    File names are changes

--- app/pages/public/Layout.tsx
+++ app/pages/public/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 import Header from "~/components/layout/Header";
-import Herocards from "~/components/pages/Hero-cards";
+import Herocards from "~/components/pages/home/Hero-cards";
 import HeroSection from "~/components/pages/home/Hero-section";
 
 export default function BaseLayout() {
